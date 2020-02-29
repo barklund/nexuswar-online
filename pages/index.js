cbe59b5b63ecb325c87036a3d30afe6f7b43cdbb
Morten Barklund <morten@barklund.dk>
2020-02-29 16:04:22 -0500

    Next.js boilerplate tailored to the project
    Added favicons for all platforms, add custom Google font and styled components. Plus deploy script.

---
+++ pages/index.js
@@ -0,0 +1,60 @@
+import Head from 'next/head'
+import styled from 'styled-components'
+
+const Home = () => (
+  <div className="container">
+    <Head>
+      <title>War of the Nexus – An X-Team X-perience</title>
+      <link rel="apple-touch-icon-precomposed" sizes="57x57" href="apple-touch-icon-57x57.png" />
+      <link rel="apple-touch-icon-precomposed" sizes="114x114" href="apple-touch-icon-114x114.png" />
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="apple-touch-icon-72x72.png" />
+      <link rel="apple-touch-icon-precomposed" sizes="144x144" href="apple-touch-icon-144x144.png" />
+      <link rel="apple-touch-icon-precomposed" sizes="60x60" href="apple-touch-icon-60x60.png" />
+      <link rel="apple-touch-icon-precomposed" sizes="120x120" href="apple-touch-icon-120x120.png" />
+      <link rel="apple-touch-icon-precomposed" sizes="76x76" href="apple-touch-icon-76x76.png" />
+      <link rel="apple-touch-icon-precomposed" sizes="152x152" href="apple-touch-icon-152x152.png" />
+      <link rel="icon" type="image/png" href="favicon-196x196.png" sizes="196x196" />
+      <link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96" />
+      <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+      <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+      <link rel="icon" type="image/png" href="favicon-128.png" sizes="128x128" />
+      <meta name="application-name" content="&nbsp;"/>
+      <meta name="msapplication-TileColor" content="#FFFFFF" />
+      <meta name="msapplication-TileImage" content="mstile-144x144.png" />
+      <meta name="msapplication-square70x70logo" content="mstile-70x70.png" />
+      <meta name="msapplication-square150x150logo" content="mstile-150x150.png" />
+      <meta name="msapplication-wide310x150logo" content="mstile-310x150.png" />
+      <meta name="msapplication-square310x310logo" content="mstile-310x310.png" />
+
+      <link href="https://fonts.googleapis.com/css?family=Expletus+Sans:400i,700&display=swap" rel="stylesheet" />
+    </Head>
+
+    <main>
+      <Title>
+        War of the Nexus!
+      </Title>
+    </main>
+
+    <style jsx global>{`
+      html,
+      body {
+        background: #333;
+        color: white;
+        padding: 0;
+        margin: 0;
+        font-family: 'Expletus Sans', -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      }
+
+      * {
+        box-sizing: border-box;
+      }
+    `}</style>
+  </div>
+)
+
+const Title = styled.h1`
+  color: white;
+`;
+
+export default Home
